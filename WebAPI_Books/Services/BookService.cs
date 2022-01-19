b8e2d70bd98ba2e6c477844b9607d3355f5f1e55
FRANCISCO SEVERINO <rickyventura271@gmail.com>
2022-01-19 11:28:02 -0400

    Agregar archivos de proyecto.

---
+++ WebAPI_Books/Services/BookService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebAPI_Books.Models;
+
+namespace WebAPI_Books.Services
+{
+
+    
+   
+    public class BookService : IBookService
+
+        {
+        public Books AddBook(Books book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Books DeleteBookByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Books GetBookByID(int Id)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpGet]
+        //Obtenemos la lista de libros
+        public  List<Books> GetBooks(Books book)
+        {
+             var url = "https://fakerestapi.azurewebsites.net/api/v1/Books";
+             HttpClient client  = new HttpClient();
+
+            return await(Books);
+        }
+
+        public Books UpdateBookByID(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ WebAPI_Books/Services/IBookService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI_Books.Models;
+
+namespace WebAPI_Books.Services
+{
+    interface IBookService
+    {
+
+
+        public List<Books> GetBooks();
+
+
+        public Books AddBook(Books book);
+
+        public  Books GetBookByID(int Id);
+
+        public Books  UpdateBookByID(int Id);
+
+        public Books  DeleteBookByID(int id);
+
+
+
+    }
+}
